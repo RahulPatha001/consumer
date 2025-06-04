b2c3f639af84f86c1c2a16aa360748d3e98bcacb
Sai Rahul Patha <rahul.patha29@gmail.com>
2025-06-04 08:04:12 +0530

    fix: fixed kafka data socket issue

--- src/main/java/com/devrahul/userservice/consumer/AuthServiceConsumer.java
+++ src/main/java/com/devrahul/userservice/consumer/AuthServiceConsumer.java
@@ -1,9 +1,13 @@
 package com.devrahul.userservice.consumer;
 
 import com.devrahul.userservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class AuthServiceConsumer {
 
     private UserRepository userRepository;
@@ -13,7 +17,7 @@ public class AuthServiceConsumer {
         this.userRepository = userRepository;
     }
 
-    @KafkaListener(groupId = "${spring.kafka.consumer.group-id}", topics = "${spring.kafka.topic-json.name}")
+    @KafkaListener(groupId = "${spring.kafka.consumer.group-id}", topics = "${spring.kafka.topic.name}")
     public void listen(Object eventData){
         try {
             System.out.println(eventData);
